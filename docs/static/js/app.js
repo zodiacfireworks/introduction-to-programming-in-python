8e9665b9784032b8f8dab22d95c9f1c3ee9310a7
zodiacfirewroks <martin.vuelta@gmail.com>
2016-12-11 17:32:20 -0500

    added owl carroulsel files, added metadata and analitics

--- docs/static/js/app.js
+++ docs/static/js/app.js
@@ -1,22 +1,46 @@
 'use strcit';
 (function ($) {
-    function fadePreloader() {
-        $('.page-wrapper').removeClass('is-loading');
+    window.fbAsyncInit = function() {
+        FB.init({
+            appId: '1183655891721937',
+            status     : true,
+            xfbml      : true,
+            version    : 'v2.8'
+        });
+        FB.AppEvents.logPageView();
+    };
 
-        setTimeout(function() {
-            $('.page-preloader').css('z-index', '-1');
-            $('.preloader-wrapper').removeClass('active');
-        }, 500);
+    function fbSDK(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {
+            return;
+        }
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    };
+
+    function googleAnalytics(i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function() {
+            (i[r].q = i[r].q || []).push(arguments)
+        };
+        i[r].l = 1 * new Date();
+        a = s.createElement(o);
+        m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m);
     };
 
     $(window).on('load', function() {
-        $(".button-collapse").sideNav({
-            menuWidth: 240,
-            edge: 'left',
-            closeOnClick: true,
-            draggable: true,
-        });
+        // Facebook Analytics
+        fbSDK(document, 'script', 'facebook-jssdk');
 
-        fadePreloader();
+        // Google Analytics
+        googleAnalytics(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+        ga('create', 'UA-88770786-1', 'auto');
+        ga('send', 'pageview');
     });
 })(jQuery);
