60d07dcbf7bcffebb221a7055596f90444626583
zodiacfirewroks <martin.vuelta@gmail.com>
2016-12-15 12:54:46 -0500

    deleted ghpages

--- scripts/hola.py
+++ scripts/hola.py
@@ -6,7 +6,7 @@ import sys
 # Definimos una fncion greeting que toma como argumento la el nombre de la
 # persona a la que queremos saludar
 def hello(name):
-    return "Hello {0}!".format(name)
+    return "Hello {0}".format(name)
 
 # Esta parte del archivo define una sección ejecutable del mismo
 # cuando enviamos el archivo al interprete, se ejecuta el código contenido en
